5e059f489af6c4edc29607ffb1154dcfe5d202df
mertdal <strafender@outlook.com>
2024-10-15 04:14:34 +0300

    Toplantı ve buluşmalarınız için bir katılımcı listesi oluşturun.

--- obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MeetingApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c990db7663f47c738d7a7aee3e28484427692eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
